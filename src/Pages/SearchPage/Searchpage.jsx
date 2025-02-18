7133dfa0c177e125c6489ca72d532e126c4852ad
anjunet <kekana.anjunet@gmail.com>
2025-02-18 23:04:10 +0200

    Fixed routing error

--- src/Pages/SearchPage/Searchpage.jsx
+++ src/Pages/SearchPage/Searchpage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Searchpage.css";
+import "./SearchPage.css";
 import { Button } from "@mui/material";
 import SearchResult from "../../Components/SearchResult/SearchResult";
 
