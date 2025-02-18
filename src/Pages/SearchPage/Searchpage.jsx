f9ca15b09eba34b40a67d0ee3ff99db905ab2ec2
anjunet <kekana.anjunet@gmail.com>
2025-02-18 22:43:41 +0200

    fixed a spelling error in a routing path

--- src/Pages/SearchPage/Searchpage.jsx
+++ src/Pages/SearchPage/Searchpage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Searchpage.CSS";
+import "./Searchpage.css";
 import { Button } from "@mui/material";
 import SearchResult from "../../Components/SearchResult/SearchResult";
 
