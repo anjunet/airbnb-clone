4cb7ff073387d9db1380c338b5e764efa4a4f461
anjunet <kekana.anjunet@gmail.com>
2025-02-18 23:16:09 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <Router>
       <>
         <Routes>
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/search" element={<Searchpage />} />
 
           <Route path="/login" element={<Login />} />
